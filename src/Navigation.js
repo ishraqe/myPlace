b3e14ea259a00f22b6e1a5c16083f1a9af48d9da
ishraqe <kanak431@diu.edu.bd>
2018-10-30 01:23:14 +0600

    issue page work started

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,10 +4,16 @@ import {
   createStackNavigator,
   createBottomTabNavigator
 } from "react-navigation";
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import {renderTabIcon} from './util';
-import {themeColor, navFontSize, appBackgroundColor, fontFamily, primaryFontColor} from './GlobalStyle';
-import CusomHeader from './components/CustomHeader';
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { renderTabIcon } from "./util";
+import {
+  themeColor,
+  navFontSize,
+  appBackgroundColor,
+  fontFamily,
+  primaryFontColor
+} from "./GlobalStyle";
+import CusomHeader from "./components/CustomHeader";
 //Screens
 import Initial from "./screens/Initial/InitialScreen";
 
@@ -18,51 +24,51 @@ import Profile from "./screens/Tab/Profile/profile";
 import Idea from "./screens/Tab/Ideas/ideas";
 import Alert from "./screens/Tab/Alerts/alert";
 // Create Issue
-import CreateIssue from './screens/Create/Create';
-
+import CreateIssue from "./screens/Create/Create";
 
 const tabNav = createBottomTabNavigator(
-    {
-      Issues: {
-        screen: Issues
-      },
-      Idea: {
-        screen: Idea
-      },
-      Create: {
-        screen: Create
-      },
-      Alert: {
-        screen: Alert
-      },
-      Profile: {
-        screen: Profile
+  {
+    Issues: {
+      screen: Issues
+    },
+    Idea: {
+      screen: Idea
+    },
+    Create: {
+      screen: Create
+    },
+    Alert: {
+      screen: Alert
+    },
+    Profile: {
+      screen: Profile
+    }
+  },
+  {
+    order: ["Issues", "Idea", "Create", "Alert", "Profile"],
+    animationEnabled: true,
+    tabBarPosition: "bottom",
+    swipeEnabled: false,
+    initialRouteName: "Issues",
+    tabBarOptions: {
+      activeTintColor: themeColor,
+      inactiveTintColor: "gray",
+      style: {
+        borderTopWidth: 0,
+        backgroundColor: "#FAFAFA"
       }
     },
-    {
-      order: ["Issues", "Idea", "Create", "Alert", "Profile"],
-      animationEnabled: true,
-      tabBarPosition: "bottom",
-      swipeEnabled: false,
-      initialRouteName: 'Create',
-      tabBarOptions: {
-        activeTintColor: themeColor,
-        inactiveTintColor: 'gray',
-        style: {
-            borderTopWidth:0,
-            backgroundColor: '#FAFAFA'
-        },
-      },
-      navigationOptions: ({ navigation }) => ({
-        header: null,
-        tabBarIcon: ({ focused, tintColor }) => {
-          const { routeName } = navigation.state;
-          return <Icon name={renderTabIcon(routeName)} size={25} color={tintColor} />;
-        },
-      })
-    }
-  );
-
+    navigationOptions: ({ navigation }) => ({
+      header: null,
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        return (
+          <Icon name={renderTabIcon(routeName)} size={25} color={tintColor} />
+        );
+      }
+    })
+  }
+);
 
 export const RootNavigation = () => {
   return createStackNavigator(
@@ -85,29 +91,27 @@ export const RootNavigation = () => {
     },
     {
       initialRouteName: "TabScreens",
-      navigationOptions: ({navigation}) => {
+      navigationOptions: ({ navigation }) => {
         return {
           headerStyle: navigationStyles.headerStyle,
-          headerTintColor: 'white'
-        }
+          headerTintColor: "white"
+        };
       }
     }
   );
 };
 
-
-
-const navigationStyles =StyleSheet.create( {
+const navigationStyles = StyleSheet.create({
   title: {
     fontSize: navFontSize,
     color: appBackgroundColor,
     fontFamily: fontFamily,
-    textAlign: 'center',
-    alignSelf: 'center',
-    flex:1
+    textAlign: "center",
+    alignSelf: "center",
+    flex: 1
   },
   headerStyle: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     elevation: 0
   }
 });
